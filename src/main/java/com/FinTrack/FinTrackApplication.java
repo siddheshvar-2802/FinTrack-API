3771fa3cf2ad6dd152411ade0b4cf5cfd0eff36c
siddheshvar-2802 <mrsiddheshvarpatil@gmail.com>
2025-09-09 22:56:13 +0530

    - Log print while application is up.

--- src/main/java/com/FinTrack/FinTrackApplication.java
+++ src/main/java/com/FinTrack/FinTrackApplication.java
@@ -1,13 +1,27 @@
 package com.FinTrack;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinTrackApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(FinTrackApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(FinTrackApplication.class, args);
+        logger.info("\n" +
+                "                                                                 \n" +
+                "   ███████╗██╗███╗   ██╗████████╗██████╗  █████╗  ██████╗██╗  ██╗ \n" +
+                "   ██╔════╝██║████╗  ██║╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝ \n" +
+                "   █████╗  ██║██╔██╗ ██║   ██║   ██████╔╝███████║██║     █████╔╝  \n" +
+                "   ██╔══╝  ██║██║╚██╗██║   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗  \n" +
+                "   ██║     ██║██║ ╚████║   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗ \n" +
+                "   ╚═╝     ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ \n" +
+                "               Financial Management Application                \n");
+
     }
 
 }
